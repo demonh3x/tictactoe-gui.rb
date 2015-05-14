a79d0ff86f01c7db8fa52bb92bd030254533b8db
Mateu Adsuara <mateuadsuara@gmail.com>
2015-05-14 20:45:24 +0100

    Removes unnecessary @s

--- lib/tictactoe/gui/qtgui/menu_gui.rb
+++ lib/tictactoe/gui/qtgui/menu_gui.rb
@@ -13,10 +13,10 @@ module Tictactoe
 
         private
         def init  
-          @widget_factory = QtGui::Widgets::Factory.new()
-          @window = @widget_factory.new_window(240, 150)
-          @game_options = @widget_factory.new_game_options(@on_configured)
-          @widget_factory.layout(@window, [@game_options])
+          widget_factory = QtGui::Widgets::Factory.new()
+          window = widget_factory.new_window(240, 150)
+          game_options = widget_factory.new_game_options(@on_configured)
+          widget_factory.layout(window, [game_options])
 
           @qt_root = @widget_factory.window.root
         end
