0716a9a3920d28b9ec9934d6cd077dcb136c42c8
Mateu Adsuara <mateuadsuara@gmail.com>
2015-05-27 11:23:18 +0100

    Reorganised Runner code

--- lib/tictactoe/gui/runner.rb
+++ lib/tictactoe/gui/runner.rb
@@ -20,42 +20,48 @@ module Tictactoe
 
       private
       attr_accessor :framework
-      attr_accessor :menu_gui, :game_gui
+      attr_accessor :menu_gui
 
       def show_windows
-        create_menu_gui
-        show_menu
+        self.menu_gui = create_menu_gui
+        menu_gui.show
       end
 
       def create_menu_gui
-        self.menu_gui = QtGui::MenuGui.new(framework)
+        menu_gui = QtGui::MenuGui.new(framework)
         menu_gui.on_configured(method(:show_game))
+
         menu_gui
       end
 
       def show_game(options)
+        create_game_window(options).show
+      end
+
+      def create_game_window(options)
         game_gui = create_game_gui(options)
-        game = create_game(options, game_gui)
+        game = create_game(options)
 
+        game.register_human_factory(human_factory(game_gui))
         Gui::GameUpdater.new(game, game_gui).receive_ticks_from(game_gui)
-        game_gui.show
-      end
 
-      def create_game(options, game_gui)
-        game = Tictactoe::Game.new(options[:board], options[:x], options[:o])
-        game.register_human_factory(lambda{|mark| HumanPlayer.new(mark).receive_moves_from(game_gui)})
-        game
-      end
-
-      def create_game_gui(options)
-        self.game_gui = QtGui::GameGui.new(framework)
-        game_gui.set_board_size(options[:board])
-        game_gui.on_play_again(method(:show_menu))
         game_gui
       end
 
-      def show_menu
-        menu_gui.show
+      def create_game_gui(options)
+        game_gui = QtGui::GameGui.new(framework)
+        game_gui.set_board_size(options[:board])
+        game_gui.on_play_again(lambda{menu_gui.show})
+
+        game_gui
+      end
+
+      def create_game(options)
+        Tictactoe::Game.new(options[:board], options[:x], options[:o])
+      end
+
+      def human_factory(game_gui)
+        lambda{|mark| HumanPlayer.new(mark).receive_moves_from(game_gui)}
       end
     end
   end
